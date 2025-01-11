65b0c08bc66f9012e059ed09acad7e628b3097bf
Emiliano Fucks <57678685+eminesf@users.noreply.github.com>
2025-01-11 04:12:11 +0000

    chore: add nova experiencia de trabalho

--- src/components/Experience/index.tsx
+++ src/components/Experience/index.tsx
@@ -3,13 +3,16 @@ import { ExperienceCard } from "../ExperienceCard";
 export const Experience: React.FC = () => (
   <section id="experience" className="parallax experience py-20 bg-gray-100">
     <div className="container mx-auto text-center parallax-content">
-      <h2 className="text-3xl font-bold text-white">Experiência</h2>
+      <h2 className="text-3xl font-bold text-white">Atual</h2>
       <div className="flex flex-wrap justify-center mt-10">
-      <ExperienceCard
+        <ExperienceCard
           position="Desenvolvedor Front-End Pleno"
           company="Creare Sistemas"
           duration="Dezembro 2024 - Atual"
         />
+      </div>
+      <h2 className="text-3xl font-bold text-white">Experiência</h2>
+      <div className="flex flex-wrap justify-center mt-10">
         <ExperienceCard
           position="Front-End Software Engineer II"
           company="Sicredi"
